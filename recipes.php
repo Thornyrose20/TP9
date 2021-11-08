413504080f6f875ee30be2d2145e2a7c2a25e2bb
Thornyrose20 <nokurose@hotmail.com>
2021-11-08 17:36:59 +0000

    completed pulling code for Brooke's Bombshell Brownies

--- recipes.php
+++ recipes.php
@@ -8,12 +8,16 @@ $list=$_GET["recipeList"];
 if($name == "Brooke's Bombshell Brownies"){
   
   if($list == "box1"){
-    
     include "ingredients.html";
     
-  }elseif ($list == "box2"){
-    
+  } elseif ($list == "box2"){
     include "equipment.html";
+    
+  } elseif ($list == "box3"){
+    include "directions.html";
+    
+  } else {
+    echo "1";
   }
     
   
@@ -21,7 +25,7 @@ if($name == "Brooke's Bombshell Brownies"){
   
   
   
-} else{
+} else {
   
   echo "0";
   

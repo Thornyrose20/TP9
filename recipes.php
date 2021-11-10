a6df5243563e44eca0afa8b77f6bee123f5c712a
Thornyrose20 <nokurose@hotmail.com>
2021-11-10 17:11:29 +0000

    added other two recipes to php file

--- recipes.php
+++ recipes.php
@@ -20,8 +20,41 @@ if($name == "Brooke's Bombshell Brownies"){
     echo "1";
   }
     
+  /* Aida's Recipe*/
+  elseif ($name == "Chocolate Chip Cookies"){
+    
+     if($list == "box1"){
+    include "ingredientsA.html";
+    
+  } elseif ($list == "box2"){
+    include "equipmentA.html";
+    
+  } elseif ($list == "box3"){
+    include "directionsA.html";
+    
+  } else {
+    echo "1";
+  }
+    
+  }
   
-  
+  /* Theo's Recipe */
+  elseif($name == "Mrs Sigg's Snickerdoodles") {
+    
+     if($list == "box1"){
+    include "ingredientsT.html";
+    
+  } elseif ($list == "box2"){
+    include "equipmentT.html";
+    
+  } elseif ($list == "box3"){
+    include "directionsT.html";
+    
+  } else {
+    echo "1";
+  }
+    
+  }
   
   
   
